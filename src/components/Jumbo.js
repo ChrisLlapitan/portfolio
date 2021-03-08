1c92c0ef1b7db03cc76a568f5af70ec4d92898e6
Christine Llapitan <christine.llapitan@tuitt.com>
2021-03-08 19:38:33 +0800

    Remove additional closing p tag on component Jumbo

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -7,7 +7,7 @@ export default function Jumbo() {
     return (
         <Jumbotron>
             <h1>Batch 101 - Hello user!</h1>
-            <p>Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Obcaecati sequi quam nisi doloremque sed, ipsam mollitia sunt consequuntur commodi asperiores delectus blanditiis dicta enim molestias, vero ad ratione voluptate. Facere.</p></p>
+            <p>Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Obcaecati sequi quam nisi doloremque sed, ipsam mollitia sunt consequuntur commodi asperiores delectus blanditiis dicta enim molestias, vero ad ratione voluptate. Facere.</p>
             <Button variant="primary">Go to blogs</Button>
         </Jumbotron>
     )
